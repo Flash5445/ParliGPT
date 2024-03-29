6df1e5b0f24ce018a9f69b83d9b3fc0d338eed6f
gurnoorsandhu <gurnoor@jediteck.com>
2024-03-29 16:10:16 -0700

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('hello world')
+import os
+import sys
+from langchain_community.document_loaders import DirectoryLoader
+import constants
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import openai
+from langchain.chat_models import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = constants.APIKEY
+
+query = sys.argv[1]
+
+
+loader = DirectoryLoader(".", glob="*.txt")
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+print(index.query(query, llm = ChatOpenAI))
